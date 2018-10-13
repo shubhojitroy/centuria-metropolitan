5402f0645d8c58cb38ae5125ec1fd3b4a1bc6631
shubhojit <royshubhojit@gmail.com>
2018-10-14 09:47:28 +1100

    Integrating Dependency injection onto registration-details and confirmation-payment comp.

--- src/app/templates/confirmation-payment/confirmation-payment.component.ts
+++ src/app/templates/confirmation-payment/confirmation-payment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { InvestorService } from '../../services/shared/investor.service';
 @Component({
   selector: 'cmo-confirmation-payment',
   templateUrl: './confirmation-payment.component.html',
@@ -8,9 +9,11 @@ import { Router } from '@angular/router';
 export class ConfirmationPaymentComponent implements OnInit {
 
   pageTitle = 'Confirmation of Application Registration';
-  constructor(private router: Router) { }
+  investors: any[];
+  constructor(private router: Router, private investorService: InvestorService) { }
 
   ngOnInit() {
+    this.investors = this.investorService.getInvestors();
   }
 
   onFinish() {
